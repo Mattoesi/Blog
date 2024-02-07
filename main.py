b6496eb823393391bf2653e27d81f4cff52be992
Mattoesi <therond.mathias@gmail.com>
2024-02-07 18:06:17 +0100

    Changed Connect to DB

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ def load_user(user_id):
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
-db = SQLAlchemy()
-db.init_app(app)
+db = SQLAlchemy(app)
+# db.init_app(app)
 
 
 # CONFIGURE TABLES
