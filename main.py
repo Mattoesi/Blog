9c47ff62b7561f879db5994992efa26e9070477c
Mattoesi <156790632+Mattoesi@users.noreply.github.com>
2024-11-30 22:52:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:////data/posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 csrf = CSRFProtect(app)
